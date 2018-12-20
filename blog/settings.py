995c0e254649cc0509637c8969fefd54e6cd08ff
DavidCasa <edwin.casa01@epn.edu.ec>
2018-12-20 10:54:38 -0500

    Mi aplicación Django Blog, primer commit

--- blog/settings.py
+++ blog/settings.py
@@ -73,15 +73,10 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
-        'USER': 'david',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
